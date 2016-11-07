bb7b0245911fe4cc74dfbebba6fee34f18664904
ezolotova <vladachka@tut.by>
2016-11-07 20:03:58 +0300

    this is commit  fourth class

---
+++ src/FourthClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ekaterina on 07.11.2016.
+ */
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
