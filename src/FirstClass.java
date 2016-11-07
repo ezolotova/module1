b49f9e3005fb6dc4e52a1205a1651f6b167a6daf
ezolotova <vladachka@tut.by>
2016-11-07 17:28:28 +0300

    this is test commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,7 +2,7 @@
  * Created by Ekaterina on 07.11.2016.
  */
 
-//test git bash не устанавливали?нет
+//test commit from idea
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
