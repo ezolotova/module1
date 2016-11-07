090a3be0a038f8b0073edf687ff67c9c113f287e
ezolotova <vladachka@tut.by>
2016-11-07 17:26:26 +0300

    first commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,7 +2,7 @@
  * Created by Ekaterina on 07.11.2016.
  */
 
-//test
+//test git bash не устанавливали?нет
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
