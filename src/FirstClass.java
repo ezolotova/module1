5618b93be120aecfa796d4cf5360b3a8305886b4
ezolotova <vladachka@tut.by>
2016-11-07 17:12:01 +0300

    init commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,8 @@
 /**
  * Created by Ekaterina on 07.11.2016.
  */
+
+//test
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Мой первый класс на Java");
